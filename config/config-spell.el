a21445842ad072d1aa2dfe3e041332910bd88cfa
vsbrm <bormyshevv@gmail.com>
2019-10-29 20:13:52 +0300

    Flyspell

--- config/config-spell.el
+++ config/config-spell.el
@@ -2,10 +2,18 @@
 ;; TODO: compose header
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;Set hunspell as a spellchecking backend
+;; Set hunspell as a spellchecking backend
 (when (executable-find "hunspell")
   (setq-default ispell-program-name "hunspell")
-  (setq ispell-really-hunspell t))
+  (setq ispell-really-hunspell t)
+  (setq ispell-dictionary "english"))
+
+;; Flyspell
+(use-package flyspell
+  :ensure t
+  :config
+  (setq flyspell-mode 1)
+  )
 
 (provide 'config-spell)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
