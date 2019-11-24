b9c4461e39c2a42c10a5511679801af3c23b0db3
vsbrm <bormyshevv@gmail.com>
2019-11-24 16:28:47 +0300

    Scrolling setup

--- config/config-editor.el
+++ config/config-editor.el
@@ -121,6 +121,11 @@
   :ensure t
   )
 
+;; Scroll configuration
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
 (provide 'config-editor)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; end editor setup
